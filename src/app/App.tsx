6d37d55f76e0f0f096e12d3b90a3b4883b714705
barbosapaulohenrique52-cmyk <barbosapaulohenrique52@gmail.com>
2026-09-19 15:52:26 -0300

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -333,6 +333,7 @@ export default function App() {
             onUpdateNinho={updateNinho}
             onSaveConfig={saveConfig}
             onViewDetails={setAveDetalheId}
+            filtro={ninhosFiltro}
           />
         )}
 
